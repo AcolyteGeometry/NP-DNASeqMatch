45c793de3c939494933e58eb2a08d2fc0002e22d
SpeaKeasY <speakeasysky@gmail.com>
2017-11-07 02:08:06 +0000

    RandDNA - Generating sequence and subseuence with overlap.

--- DNASeq/DNARand/RandDNA.py
+++ DNASeq/DNARand/RandDNA.py
@@ -1,20 +1,63 @@
 ## @package: DNASeq.DNARand.RandDNA
 #  Package for randomizing DNASequences for testing
 from DNASeq.DNASequence import DNASequence
+from Utils.MathHelpers import MathHelpers
 from .RandBits import RandBits
+from numpy.random import randint
+import numpy as np
 
 
 ## Class for constructing random main DNASequence and DNASequences with overlapping segments
 class RandDNA():
 
     rbits = RandBits()
+    mathh = MathHelpers()
 
     ## The constructor
     def __init__(self):
         pass
 
+    ## Generates a series of partially overlapping DNASequence objects
+    #  @type self: RandDNA
+    #  @param: The object
+    #
+    #  @type length: int
+    #  @param length: The langth of the overall sequence bp
+    #
+    #  @type fraglen: int
+    #  @param fraglen: The length of the sequence fragments bp
+    #
+    #  @type fragvar: float
+    #  @param fragvar: Normal variation of the fragment length (0.0 - 1.0 [0-100%])
+    #
+    #  @type fragovmin: float
+    #  @param fragovmin: The minimum normal overlap of fragment
+    #
+    #  @type fragovmax: float
+    #  @param fragovmax: The maximum normal overlap of fragment
+    #
+    #  @rtype: np.array(bitarray, np.array(bitarray, bitarray, ...))
+    #  @return: Array containing DNASequence complete sequence and array of DNASequence objects
+    #  of overlapping subseuences.
+    def getDNASeqSeries(self, length = 100, fraglen = 20, fragvar = 0.05, fragovmin = 0.1, fragovmax = 0.2):
+        farrl = self.mathh.floatToIntUp((length / fraglen) / fragovmax) # Array size for output with overlap rounded up
+        ret = np.array([None, np.array([None * farrl])]) # Initialize array
+        ret.flags.writeable = True # Make arrays writable
+        ret[1].flags.writeable = True
+        ret[0] = self.getRandDNASequence(length) # Generate full sequence
+        flen = randint(self.mathh.floatToInt(fraglen - (fraglen * fragvar)), \
+                       self.mathh.floatToInt(fraglen + (fraglen * fragvar)), farrl) # Fragment lengthts
+        folen = randint(self.mathh.floatToInt(fraglen * fragovmin),\
+                        self.mathh.floatToInt(fraglen * fragovmax), farrl - 1) # Fragment overlap for each length -> next length
+        ret[1][0] = ret[0].getDNASubseqBits(0, flen[0]) # Get initial fragment
+        idx = flen[0]
+        for i in range(1, farrl, 1): # Get fragments sequence with overlap
+            ret[1][i] = ret[0].getDNASubseqBits((idx - folen[i]), flen[i])
+            idx += flen[i]
+        return ret
+
     ## Generates a random DNASequence
-    #  @type self: RandBits
+    #  @type self: RandDNA
     #  @param self: The object
     #
     #  @type length: int
--- Utils/MathHelpers.py
+++ Utils/MathHelpers.py
@@ -80,3 +80,28 @@ class MathHelpers():
         if((ret % 2) != 0):
             ret -= 1
         return ret
+
+    ## Converts a float to an int rounded up
+    #  @type self: MathHelpers
+    #  @param self: The object
+    #
+    #  @type fl: float, string
+    #  @param fl: Float to be converted to integer
+    #
+    #  @rtype: int
+    #  @return: An integer rounded up from to the nearest whole number
+    def floatToIntUp(self, fl):
+        if(isinstance(fl, float)): # If this is a float
+            if(fl.is_integer()): # If it is a whole number return as integer;
+                return int(fl)
+            else: # Otherwise split at the decimal.
+                intarr = str(fl).split(".")
+                if(len(intarr) == 1 & int(intarr[1]) != 0): # Read 1/10ths place, check if >= 5.
+                    return int(intarr[0]) + 1 # Return integer rounded up.
+                else:
+                    return int(intarr[0]) # Otherwise return integer rounded down.
+        elif(isinstance(fl, str)): # If float is a string, convert to float and recurse.
+            return self.floatToInt(float(fl))
+        elif(isinstance(fl, int)): # If float is already an integer just return it.
+            return fl
+        return 0 # Return 0 if the float is invalid.
